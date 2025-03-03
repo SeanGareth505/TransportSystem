f3b9223019c10da55e297574d81b14efcc00df87
SeanGareth <101504237+SeanGareth505@users.noreply.github.com>
2025-03-03 09:45:21 +0200

    Initial push

---
+++ src/app/layout/component/app.footer.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    standalone: true,
+    selector: 'app-footer',
+    template: `<div class="layout-footer">
+        Transport Management System © 2021
+    </div>`
+})
+export class AppFooter {}
